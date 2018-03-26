0acf848d25ff6da18d77c18b338c15631deadd4d
kgtkr <kgtkr.jp@gmail.com>
2018-03-26 11:41:58 +0900

    動いた

---
+++ src/main.rs
@@ -0,0 +1,104 @@
+use std::io::{self, Read, Write};
+
+fn main() {
+    run(to_ast(&mut to_token(
+        "+++++++++[>++++++++>+++++++++++>+++++<<<-]>.>++.+++++++..+
+++.>-.------------.<++++++++.--------.+++.------.--------.>+.",
+    ).into_iter()));
+}
+
+fn run(code: Vec<AST>) {
+    let mut mem = [0u8; 20000];
+    let mut ptr = 0;
+
+    run_code(code, &mut mem, &mut ptr);
+}
+
+fn run_code(code: Vec<AST>, mem: &mut [u8; 20000], ptr: &mut usize) {
+    for x in code {
+        match x {
+            AST::PtrInc => {
+                *ptr += 1;
+            }
+            AST::PtrDec => {
+                *ptr -= 1;
+            }
+            AST::ValInc => {
+                mem[*ptr] += 1;
+            }
+            AST::ValDec => {
+                mem[*ptr] -= 1;
+            }
+            AST::Out => {
+                print!("{}", char::from(mem[*ptr]));
+            }
+            AST::In => {
+                let mut buffer = [0; 1];
+                std::io::stdin().read(&mut buffer[..]).unwrap();
+                mem[*ptr] = buffer[0];
+            }
+            AST::Loop(v) => while mem[*ptr] != 0 {
+                run_code(v.clone(), mem, ptr);
+            },
+        }
+    }
+}
+
+fn to_token(s: &str) -> Vec<Token> {
+    s.chars()
+        .filter_map(|x| match x {
+            '>' => Some(Token::PtrInc),
+            '<' => Some(Token::PtrDec),
+            '+' => Some(Token::ValInc),
+            '-' => Some(Token::ValDec),
+            '.' => Some(Token::Out),
+            ',' => Some(Token::In),
+            '[' => Some(Token::LoopBegin),
+            ']' => Some(Token::LoopEnd),
+            _ => None,
+        })
+        .collect()
+}
+
+fn to_ast<T: Iterator<Item = Token>>(ts: &mut T) -> Vec<AST> {
+    let mut r = Vec::new();
+    while let Some(t) = ts.next() {
+        let v = match t {
+            Token::PtrInc => AST::PtrInc,
+            Token::PtrDec => AST::PtrDec,
+            Token::ValInc => AST::ValInc,
+            Token::ValDec => AST::ValDec,
+            Token::Out => AST::Out,
+            Token::In => AST::In,
+            Token::LoopBegin => AST::Loop(to_ast(ts)),
+            Token::LoopEnd => {
+                return r;
+            }
+        };
+        r.push(v);
+    }
+    r
+}
+
+#[derive(PartialEq, Debug, Clone)]
+enum Token {
+    PtrInc,
+    PtrDec,
+    ValInc,
+    ValDec,
+    Out,
+    In,
+    LoopBegin,
+    LoopEnd,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+enum AST {
+    PtrInc,
+    PtrDec,
+    ValInc,
+    ValDec,
+    Out,
+    In,
+    Loop(Vec<AST>),
+}
